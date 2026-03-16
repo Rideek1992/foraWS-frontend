4e8e6281873e42cebba48af1ddab619753fa7cbe
Damian Rabiński <damian.rabinski@fora-poligrafia.pl>
2026-03-16 21:03:51 +0100

    start building data-analitics site, i have hero, dataSection and Scope section

--- src/app/features/data-analysis/data-analysis.component.ts
+++ src/app/features/data-analysis/data-analysis.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HeroComponent} from "../../shared/components/hero/hero.component";
+import {ApibackandService} from "../../core/apibackand.service";
+import {
+  ScopeServiceComponent
+} from "../../shared/components/scope-service/scope-service.component";
 
 @Component({
   selector: 'app-data-analysis',
   standalone: true,
-  imports: [],
+  imports: [
+    HeroComponent,
+    ScopeServiceComponent
+  ],
   templateUrl: './data-analysis.component.html',
   styleUrl: './data-analysis.component.sass'
 })
-export class DataAnalysisComponent {
+export class DataAnalysisComponent implements OnInit {
 
+
+
+  constructor(private api: ApibackandService){}
+
+  dataAnalitictScopec:any =[]
+
+  ngOnInit() {
+     this.api.getGraphicElements('data-analytics').subscribe({
+       next: (data) => {
+         this.dataAnalitictScopec = data.data
+       },
+       error: (err) =>{
+         error: (err.error.message)
+       }
+     })
+  }
 }
