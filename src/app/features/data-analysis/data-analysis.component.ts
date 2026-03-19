c960862be73310c648bdf43d3f547675fe22f511
Damian Rabiński <damian.rabinski@fora-poligrafia.pl>
2026-03-19 12:56:36 +0100

    finish desctop view from data-analitics page

--- src/app/features/data-analysis/data-analysis.component.ts
+++ src/app/features/data-analysis/data-analysis.component.ts
@@ -4,13 +4,19 @@ import {ApibackandService} from "../../core/apibackand.service";
 import {
   ScopeServiceComponent
 } from "../../shared/components/scope-service/scope-service.component";
+import {
+  ButtonLinkComponent
+} from "../../shared/uploadComponents/button-link/button-link.component";
+import {JsonPipe} from "@angular/common";
 
 @Component({
   selector: 'app-data-analysis',
   standalone: true,
   imports: [
     HeroComponent,
-    ScopeServiceComponent
+    ScopeServiceComponent,
+    ButtonLinkComponent,
+    JsonPipe
   ],
   templateUrl: './data-analysis.component.html',
   styleUrl: './data-analysis.component.sass'
@@ -22,6 +28,7 @@ export class DataAnalysisComponent implements OnInit {
   constructor(private api: ApibackandService){}
 
   dataAnalitictScopec:any =[]
+  dataAnaliticsWhyWe:any =[]
 
   ngOnInit() {
      this.api.getGraphicElements('data-analytics').subscribe({
@@ -32,5 +39,14 @@ export class DataAnalysisComponent implements OnInit {
          error: (err.error.message)
        }
      })
+
+    this.api.getSiteMarkingWarehouse('analitics_why_we').subscribe({
+      next: (data) =>{
+        this.dataAnaliticsWhyWe = data.data.map((item:any) => item.analitics_why_we)
+      },
+      error: (err) =>{
+        console.log(err.error.message)
+      }
+    })
   }
 }
